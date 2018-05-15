af04a860cd00092816c470b26b1e0b86ec54debc
joshuagibeonm <joshuagibeon7719@gmail.com>
2018-05-16 00:51:10 +0700

    Create Structure for ai.php

---
+++ ai.php
@@ -0,0 +1,32 @@
+<?php
+
+    function heuristicFunction(){
+
+    }
+
+    for($i = 1; $i <=9; $i++){
+        $name = "\$_POST[\'b".$i."\']";
+
+        $box[$i] = $$name;
+    }
+
+    for($i = 1; $i <= 9; $i++){
+        if($box[$i] == 0){
+            $available[] = $box[$i];
+        }
+    }
+    
+    $maxPoint = -1;
+    $choice = -1;
+    foreach ($available as $key => $value) {
+        $heuristicPoint[$value] = heuristicFunction();
+        if ($maxPoint < $heuristicPoint[$value]){
+            $choice = $key;
+            $maxPoint = $heuristicPoint[$value];
+        }
+    }
+
+    $returnObject->answer = $choice;
+    $JSON_Object = json_encode($returnObject);
+    return $JSON_Object;
+?>
